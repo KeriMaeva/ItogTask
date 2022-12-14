865a56d12c91e15116f052d84bb847ae093339ba
KeriMaeva <Karinchikmaeva@bk.ru>
2022-12-14 13:20:01 +0300

    Написали программу

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿void CalcArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array1 = new string[] {"hello", "2", "world", ":-)", "1234", "1567", "-2", "Computer science", "Russia", "Denmark", "Kazan", "YES"};
+string[] array2 = new string[array1.Length];
+CalcArray(array1, array2);
+PrintArray(array2);
